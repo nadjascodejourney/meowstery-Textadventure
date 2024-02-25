1f02e0be632f53dd2b51feb410c6506f33a097f2
Nadja Probst <nadja.schwarzenegger@gmail.com>
2024-02-25 20:29:47 +0100

    funktionalität verbessert

--- data/templates.js
+++ data/templates.js
@@ -1,4 +1,4 @@
-// import { colors, symbols } from "./colors.js";
+import { colors } from "./colors.js";
 
 export const catTemplate = `
     *      ,ooooooo.            *
@@ -26,4 +26,13 @@ _/\\_   _/\\_/\\_/\\_/\\_/\\_/\\_/\\_/\\_/\\_/\\_/\\_/\\
  A catastic journey through time and space
 `;
 
-// module.exports = { catTemplate };
+export const mainMenuTemplate = `
+0=================___Hauptmenü___===================0
+0                                                   0                                                          
+0   Tippe ${colors.b}P${colors.reset} für ein neues Abenteuer!                0
+0   Tippe ${colors.g}L${colors.reset} um mehr über Luna zu erfahren           0
+0   Tippe ${colors.y}I${colors.reset} für mehr Info über das Spiel            0
+0   Tippe ${colors.m}B${colors.reset} zum Beenden des Spiels                  0
+0                                                   0
+0===================================================0
+`;
--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 //>> Projekt aufsetzen:
 
 import readlineSync from "readline-sync";
-import { catTemplate } from "./data/templates.js";
+import { catTemplate, mainMenuTemplate } from "./data/templates.js";
 import { colors } from "./data/colors.js";
 // import symbols from "./data/symbols";
 
 // JSON file bzw. Daten der Geschichte mit ES Modules Node einlesen:
 // 1.
 import { readFile } from "fs/promises";
+import { info } from "console";
 // 2.
 async function readJSONFile(filePath) {
   try {
@@ -21,7 +22,9 @@ async function readJSONFile(filePath) {
 // 3.
 const data = await readJSONFile("./story.json");
 // 4.
-const { title, subtitle, version, story: storyArray } = data;
+const { story: storyArray } = data;
+
+const infotext = data.meta.infotext;
 
 // .................................
 // .................................
@@ -29,22 +32,15 @@ const { title, subtitle, version, story: storyArray } = data;
 //>> Text-Formatierungsfunktion
 // Erläuterung siehe readme.md
 
-function formatText(text, maxLength /* maxLines */) {
+function formatText(text, maxLength) {
   const words = text.split(" ");
   let formattedText = "";
   let lineLength = 0;
-  // let lineCount = 0;
 
   words.forEach((word) => {
     if (lineLength + word.length + 1 > maxLength) {
       formattedText += "\n"; // man könnte hier noch weitere Zeile Abstand eingeben
       lineLength = 0;
-      // lineCount++;
-
-      /*  if (lineCount >= maxLines) {
-        formattedText += "\n\n";
-        lineCount = 0;
-      } */
     }
     formattedText += word + " ";
     lineLength += word.length + 1;
@@ -54,91 +50,135 @@ function formatText(text, maxLength /* maxLines */) {
 // .................................
 // .................................
 
+//>> START DES PROGRAMMS
+
 function startTextAdventure() {
-  // const startScreen = readlineSync.question(catTemplate); // question clg automatically
   console.log(catTemplate);
   const pressStart = readlineSync.question(
-    `\n${colors.c}Drücke ENTER, um das Spiel zu beginnen. ${colors.reset}`
+    `\n${colors.c}Drücke ENTER. ${colors.reset}`
   );
-  console.clear(); //! funktioniert bisher nicht wie erwartet
-  //? Muss ich die Textauswahlfunktionen (findID und findText) vllt in einen eigenen Scope verschachteln?
+  console.clear();
 
-  //>> findID Funktion
-  //* Findet Auswahlmöglichkeiten für eine bestimmte Situation in der Story anhand der ID des jeweiligen Objekts und gibt sie zurück.
-  function findId(searchedId) {
-    for (let i = 0; i < storyArray.length; i++) {
-      if (storyArray[i].id == searchedId) {
-        return storyArray[i].choice;
-      }
-    }
-  }
-  // call erfolgt erst in findText()!
+  const mainMenu = readlineSync.question(mainMenuTemplate);
+  console.clear();
 
-  //>> 2. findText Funktion
-  //* Findet Text und Auswahlmöglichkeiten für eine bestimmte Situation in der Story, zeigt diese an und fordert Benutzer auf, Entscheidung zu treffen.
-  function findText(searched) {
-    let text;
-    let amountOfElementsInLinksArray;
-    for (let x = 0; x < storyArray.length; x++) {
-      if (storyArray[x].id == searched) {
-        // Test
-        // console.log("Text aus storyArray[x].text:", storyArray[x].text);
+  switch (mainMenu.toLowerCase()) {
+    case "p":
+      console.log("Starte das Spiel...");
 
-        text = storyArray[x].text + "\n\n";
+      //>> 1. Funktion für Situationen, 2. Funktion für Text und Auswahlmöglichkeiten
+      //% 1. findID Funktion
 
-        amountOfElementsInLinksArray = storyArray[x].thisLinksTo.length;
-
-        for (let z = 0; z < amountOfElementsInLinksArray; z++) {
-          let goTo = storyArray[x].thisLinksTo[z];
-
-          let situationalText = findId(goTo);
-
-          // Test
-          // console.log("situationalText:", situationalText);
-          // console.log("Aktueller Wert von text vor der Zeile:", text);
-
-          situationalText = situationalText.join(" ");
-
-          //% Textformatierung Situationstext und Maximale Zeilenlänge
-          situationalText = formatText(situationalText, 60); //! Seltsames Verhalten, da Text unerwartet umbricht und scheinbar nicht auf Zeilenlänge reagiert
-
-          text =
-            text +
-            `${colors.g} > ${situationalText} ${colors.reset}` +
-            `\n${colors.y}(Wähle ${z})${colors.reset} \n\n`;
+      //* Findet bestimmte Situation in der Story anhand der ID des jeweiligen Objekts und gibt sie zurück.
+      function findId(searchedId) {
+        for (let i = 0; i < storyArray.length; i++) {
+          if (storyArray[i].id == searchedId) {
+            return storyArray[i].choice;
+          }
         }
-        break;
       }
-    }
-    //% Textformatierung Haupttext + Maximale Zeilenlänge, ggf. lineCount (aktuell deaktiviert)
-    text = formatText(text, 80 /* 5 */);
-    console.log("\n>" + text + "\n");
+      //! call erfolgt erst in findText()!
 
-    // check User Input
-    // ? Soll ich hier lieber eine eigene Funktion schreiben, die ich dann hier aufrufe?
-    // ? Soll der UserInput Code weiter oben stehen?
+      //% 2. findText Funktion
 
-    const userInput = readlineSync.question(
-      `\n${colors.c}Triff eine Entscheidung: ${colors.reset}`
-    );
-    console.clear(); // Bisherige Ausgabe in der Konsole löschen, wenn User eine Entscheidung getroffen hat
-    //! funktioniert aber bisher nur beim Text, nicht beim template
+      //* Findet Text und Auswahlmöglichkeiten für eine bestimmte Situation in der Story, zeigt diese an und fordert Benutzer auf, Entscheidung zu treffen.
+      function findText(searched) {
+        let text;
+        let amountOfElementsInLinksArray;
 
-    const userDecision = parseInt(userInput);
-    if (
-      !isNaN(userDecision) &&
-      userDecision >= 0 &&
-      userDecision < amountOfElementsInLinksArray
-    ) {
-      findText(storyArray[searched].thisLinksTo[userDecision]);
-    } else {
-      -console.log(
-        `${colors.m}Katzenminze im Kopf? Bitte versuche es erneut und wähle eine gültige Option.${colors.reset} `
-      );
-      findText(searched);
-    }
+        for (let x = 0; x < storyArray.length; x++) {
+          if (storyArray[x].id == searched) {
+            // Test
+            // console.log("Text aus storyArray[x].text:", storyArray[x].text);
+
+            text = storyArray[x].text + "\n\n";
+
+            amountOfElementsInLinksArray = storyArray[x].thisLinksTo.length;
+
+            for (let z = 0; z < amountOfElementsInLinksArray; z++) {
+              let goTo = storyArray[x].thisLinksTo[z];
+
+              let situationalText = findId(goTo);
+
+              // Test
+              // console.log("situationalText:", situationalText);
+              // console.log("Aktueller Wert von text vor der Zeile:", text);
+
+              situationalText = situationalText.join(" ");
+
+              //% Textformatierung Situationstext und Maximale Zeilenlänge
+              situationalText = formatText(situationalText, 80); //! Seltsames Verhalten, da Text unerwartet umbricht und scheinbar nicht auf Zeilenlänge reagiert
+
+              text =
+                text +
+                `${colors.g} > ${situationalText} ${colors.reset}` +
+                `\n${colors.y}(Wähle ${z})${colors.reset} \n\n`;
+            }
+            break;
+          }
+        }
+        //% Textformatierung Haupttext + Maximale Zeilenlänge
+        text = formatText(text, 80);
+        console.log("\n> " + text + "\n");
+
+        // Überprüfung, ob Auswahlmöglichkeiten vorhanden sind
+        if (amountOfElementsInLinksArray === 0) {
+          const restartChoice = readlineSync.question(
+            `${colors.r} Wenn du möchtest, kannst du nochmal von vorne beginnen und Luna auf einem anderen Verlauf der Geschichte folgen (Ja/Nein).
+            Du kannst das Spiel auch beenden indem du Strg + C drückst. ${colors.reset}`
+          );
+
+          if (restartChoice.toLowerCase() === "ja") {
+            console.clear();
+            startTextAdventure();
+            return;
+          } else {
+            console.log(
+              `${colors.y}Vielen Dank fürs Spielen! Bis zum nächsten Mal!${colors.reset}`
+            );
+            return;
+          }
+        } else {
+          // check user Input, wenn Auswahlmöglichkeiten vorhanden
+          const userInput = readlineSync.question(
+            `\n${colors.c}Triff eine Entscheidung ${colors.reset}`
+          );
+          console.clear();
+
+          const userDecision = parseInt(userInput);
+          if (
+            !isNaN(userDecision) &&
+            userDecision >= 0 &&
+            userDecision < amountOfElementsInLinksArray
+          ) {
+            findText(storyArray[searched].thisLinksTo[userDecision]);
+          } else {
+            console.log(
+              `${colors.m}Katzenminze im Kopf? Bitte versuche es erneut und wähle eine gültige Option.${colors.reset} `
+            );
+            findText(searched); // damit Spieler erneut wählen kann
+          }
+        }
+      }
+      findText(0);
+
+      break;
+    case "l":
+      console.log("Luna wird vorgestellt...");
+      break;
+    case "i":
+      let gameinfo = formatText(infotext, 100);
+      console.log("Über das Spiel...\n\n" + gameinfo + "\n");
+
+      break;
+    case "b":
+      console.log("Beende das Spiel...");
+      return;
+    default:
+      console.log("Katzenminze im Kopf?. Bitte versuche es erneut.");
+      startTextAdventure();
+      return;
   }
-  findText(0);
 }
 startTextAdventure();
 // .....................................

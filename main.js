edf99e70f5dd1f80009ef792d5fc4d9e582a174d
Nadja Probst <nadja.schwarzenegger@gmail.com>
2024-02-25 15:00:06 +0100

    Schriftfarben eingebaut

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 import readlineSync from "readline-sync";
 import { catTemplate } from "./data/templates.js";
-// import colors from "./data/colors";
+import { colors } from "./data/colors.js";
 // import symbols from "./data/symbols";
 
 // JSON file bzw. Daten der Geschichte mit ES Modules Node einlesen:
@@ -24,19 +24,27 @@ const data = await readJSONFile("./story.json");
 const { title, subtitle, version, story: storyArray } = data;
 
 // .................................
+// .................................
 
 //>> Text-Formatierungsfunktion
 // Erläuterung siehe readme.md
 
-function formatText(text, maxLength) {
+function formatText(text, maxLength /* maxLines */) {
   const words = text.split(" ");
   let formattedText = "";
   let lineLength = 0;
+  // let lineCount = 0;
 
   words.forEach((word) => {
     if (lineLength + word.length + 1 > maxLength) {
-      formattedText += "\n";
+      formattedText += "\n"; // man könnte hier noch weitere Zeile Abstand eingeben
       lineLength = 0;
+      // lineCount++;
+
+      /*  if (lineCount >= maxLines) {
+        formattedText += "\n\n";
+        lineCount = 0;
+      } */
     }
     formattedText += word + " ";
     lineLength += word.length + 1;
@@ -44,9 +52,16 @@ function formatText(text, maxLength) {
   return formattedText;
 }
 // .................................
+// .................................
 
 function startTextAdventure() {
-  const startScreen = readlineSync.question(catTemplate); // question clg automatically
+  // const startScreen = readlineSync.question(catTemplate); // question clg automatically
+  console.log(catTemplate);
+  const pressStart = readlineSync.question(
+    `\n${colors.c}Drücke ENTER, um das Spiel zu beginnen. ${colors.reset}`
+  );
+  console.clear(); //! funktioniert bisher nicht wie erwartet
+  //? Muss ich die Textauswahlfunktionen (findID und findText) vllt in einen eigenen Scope verschachteln?
 
   //>> findID Funktion
   //* Findet Auswahlmöglichkeiten für eine bestimmte Situation in der Story anhand der ID des jeweiligen Objekts und gibt sie zurück.
@@ -85,23 +100,27 @@ function startTextAdventure() {
           situationalText = situationalText.join(" ");
 
           //% Textformatierung Situationstext und Maximale Zeilenlänge
-          situationalText = formatText(situationalText, 60);
+          situationalText = formatText(situationalText, 60); //! Seltsames Verhalten, da Text unerwartet umbricht und scheinbar nicht auf Zeilenlänge reagiert
 
-          text = text + ">" + situationalText + " (Wähle " + z + ")\n\n";
+          text =
+            text +
+            `${colors.g} > ${situationalText} ${colors.reset}` +
+            `\n${colors.y}(Wähle ${z})${colors.reset} \n\n`;
         }
         break;
       }
     }
-    //% Textformatierung Haupttext + Maximale Zeilenlänge
-    text = formatText(text, 80);
-
+    //% Textformatierung Haupttext + Maximale Zeilenlänge, ggf. lineCount (aktuell deaktiviert)
+    text = formatText(text, 80 /* 5 */);
     console.log("\n>" + text + "\n");
 
     // check User Input
     // ? Soll ich hier lieber eine eigene Funktion schreiben, die ich dann hier aufrufe?
     // ? Soll der UserInput Code weiter oben stehen?
 
-    const userInput = readlineSync.question("Triff eine Entscheidung:");
+    const userInput = readlineSync.question(
+      `\n${colors.c}Triff eine Entscheidung: ${colors.reset}`
+    );
     console.clear(); // Bisherige Ausgabe in der Konsole löschen, wenn User eine Entscheidung getroffen hat
     //! funktioniert aber bisher nur beim Text, nicht beim template
 
@@ -113,8 +132,8 @@ function startTextAdventure() {
     ) {
       findText(storyArray[searched].thisLinksTo[userDecision]);
     } else {
-      console.log(
-        `Katzenminze im Kopf? Bitte versuche es erneut und wähle eine gültige Option.`
+      -console.log(
+        `${colors.m}Katzenminze im Kopf? Bitte versuche es erneut und wähle eine gültige Option.${colors.reset} `
       );
       findText(searched);
     }
